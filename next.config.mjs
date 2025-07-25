4da34394c6f9f6611258f129ecf6f1df03aa5737
WinPyaeKhin054734 <winpyaepyaekhin.110@gmail.com>
2025-07-25 21:33:54 +0630

    Fix: remove lightningcss postinstall and disable optimizeCss

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+     experimental: {
+    optimizeCss: false // turn off lightningcss
+  }
+};
 
 export default nextConfig;
+
+
+
+
